472608e42e4a74c8fec9edc73c8782d4eb21b343
cstaker <Cody@Desktop>
2014-01-25 15:15:34 -0800

    intial commit

--- src/checkers/Checkers.java
+++ src/checkers/Checkers.java
@@ -15,6 +15,7 @@ public class Checkers {
     /**
      * @param args the command line arguments
      */
+   
     public static void main(String[] args) {
         // TODO code application logic here
     }
