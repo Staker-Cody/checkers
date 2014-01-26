534f0c45fd89390482c74450833f649de58c0016
cstaker <Cody@Desktop>
2014-01-25 19:39:38 -0800

    Board and Pieces add

---
+++ src/checkers/Pieces .java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package checkers;
+
+/**
+ *
+ * @author Cody
+ */
+public class Pieces {
+    public int blackPieces = 12;
+    public int whitePieces = 12;
+    
+    
+    
+}
