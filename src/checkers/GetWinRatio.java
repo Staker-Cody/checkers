800f127bc65756afac7187a190f40a125237a0b1
Austin Owens <austin@perfectcu.be>
2014-02-01 16:25:52 -0800

    win ratio yay

--- src/checkers/GetWinRatio.java
+++ src/checkers/GetWinRatio.java
@@ -3,8 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import java.util.Scanner;
+
 package checkers;
+
+import java.util.Scanner;
+
 /**
  *
  * @author Austin
@@ -42,4 +45,4 @@ public class GetWinRatio {
         winRatio = wins1/totalGames1;
         System.out.println(winRatio);
         
-    }
+    }}
