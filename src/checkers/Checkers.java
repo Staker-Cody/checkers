f7b6637c7eb8b5fb703d7ffe755a14f21193edfc
cstaker <Cody@Desktop>
2014-02-06 13:57:42 -0800

    just a little organization

--- src/checkers/Checkers.java
+++ src/checkers/Checkers.java
@@ -17,16 +17,13 @@ public class Checkers {
     /**
      * @param args the command line arguments
      */
-    String player1Name;
-    String player2Name;
-    String instructions = "instructions";
+ 
     
     public static void main(String[] args) {
         Checkers myGame = new Checkers();
-        myGame.getName();
-        myGame.displayHelp();
+      
         MainMenu menu = new MainMenu();
-       
+        menu.getName();
         Board dBoard = new Board();
         dBoard.displayBoard();
         Pieces Pieces = new Pieces();
@@ -35,18 +32,6 @@ public class Checkers {
         newRatio.getWinRatio();
     }
     
-    public void getName() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Player 1 Enter your name: ");
-        this.player1Name = input.next();
-        System.out.println("Player 2 Enter your name: ");
-        this.player2Name = input.next();
-    }
-    
-    public void displayHelp() {
-        System.out.println("\nWelcome " + this.player1Name + " and "+ this.player2Name + "\n");
-        System.out.println(this.instructions);       
-    }
 
 
 }
--- src/checkers/MainMenu.java
+++ src/checkers/MainMenu.java
@@ -6,12 +6,32 @@
 
 package checkers;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Cody
  */
+  
 public class MainMenu {
 
+    String player1Name;
+    String player2Name;
+    String instructions = "instructions";
    
+    public void getName() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Player 1 Enter your name: ");
+        this.player1Name = input.next();
+        System.out.println("Player 2 Enter your name: ");
+        this.player2Name = input.next();
+        System.out.println("\nWelcome to Checkers" + this.player1Name + " and "+ this.player2Name + "\n");
+        System.out.println(this.instructions);   
+    }
+    public void menu(){
+        
+        
+    }
     
 }
+    
